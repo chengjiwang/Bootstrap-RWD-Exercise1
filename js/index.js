fae54e7ddaebde9ff41ec49014b23b620fb7e66a
chengjiwang <chengji3728@gmail.com>
2016-02-19 23:08:03 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.nav a').click(function( ){
+		$('.navbar-collapse').toggleClass('in');
+	}); //end click 
+
+     $('.flexslider').flexslider({
+        animation: "slide"
+      });
+});
+
