c9f8670ff16f8cdbe3b0b0fcb09a8d025002fd99
chengjiwang <chengji3728@gmail.com>
2016-02-24 21:34:37 +0800

    modified artist

--- js/custom.js
+++ js/custom.js
@@ -4,31 +4,41 @@ $(document).ready(function(){
 	}); //end click 
 
     $("#main-slider").owlCarousel({
- 
+      // autoPlay: 5000
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
+      responsive: true,
       singleItem:true
+
  
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+      
  
     }); // end main-slider
 
-    $("#owl-demo").owlCarousel({
+    // $("#owl-demo").owlCarousel({
      
-        // autoPlay: 3000, //Set AutoPlay to 3 seconds
+    //     // autoPlay: 3000, //Set AutoPlay to 3 seconds
 
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3],
-        itemsTablet :[768,2],
-        itemsMobile: [479,1]
+    //     items : 4,
+    //     itemsDesktop : [1199,3],
+    //     itemsDesktopSmall : [979,3],
+    //     itemsTablet :[768,2],
+    //     itemsMobile: [479,1]
      
-    });
+    // });
+
+    $("#owl-artist").owlCarousel({
+    items : 4,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3],
+    itemsTablet :[768,2],
+    itemsMobile: [479,1],
+    navigation : true,
+    navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    pagination : false, //分頁
+    paginationSpeed : 800,
+    slideSpeed : 700
+  });
 });
 
