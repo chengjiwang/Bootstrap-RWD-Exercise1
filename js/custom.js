f60a4581dac727f567181e587e693827d5687300
chengjiwang <chengji3728@gmail.com>
2016-02-26 21:20:33 +0800

    modified rwd and navigationText

--- js/custom.js
+++ js/custom.js
@@ -4,29 +4,16 @@ $(document).ready(function(){
 	}); //end click 
 
     $("#main-slider").owlCarousel({
-      // autoPlay: 5000
+       // autoPlay: 5000,
       navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
+      navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      slideSpeed : 500,
+      pagination : false,
+      paginationSpeed : 500,
       responsive: true,
       singleItem:true
-
- 
-      
- 
     }); // end main-slider
 
-    // $("#owl-demo").owlCarousel({
-     
-    //     // autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-    //     items : 4,
-    //     itemsDesktop : [1199,3],
-    //     itemsDesktopSmall : [979,3],
-    //     itemsTablet :[768,2],
-    //     itemsMobile: [479,1]
-     
-    // });
 
     $("#owl-artist").owlCarousel({
     items : 4,
@@ -37,7 +24,7 @@ $(document).ready(function(){
     navigation : true,
     navigationText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
     pagination : false, //分頁
-    paginationSpeed : 800,
+    // paginationSpeed : 800,
     slideSpeed : 700
   });
 });
