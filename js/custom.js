45fb5146685c6d8a0e23a339f16633521e92c55e
chengjiwang <chengji3728@gmail.com>
2016-08-02 11:57:36 +0800

    add animation

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,40 @@
 $(document).ready(function() {
+    var $animation_elements = $('.animation-element'),
+        $window = $(window);
+    function checkView() {
+        var window_height = $window.height(),
+            window_top_position = $window.scrollTop(),  
+            window_bottom_position = (window_top_position + window_height);
+     
+    $.each($animation_elements, function() {
+        var $element = $(this),
+            element_height = $element.outerHeight(), 
+            element_top_position = $element.offset().top, 
+            element_bottom_position = (element_top_position + element_height);
+          
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } 
+      });
+    }
+    $window.on('scroll resize', checkView);
+    $window.trigger('scroll');
+
     $('#menu-nav a').click(function() {
         $('#menu-nav').toggleClass('in');
     }); //end click 
 
     $('#menu-nav a').on('click', function() {
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 20
+            scrollTop: $(this.hash).offset().top - 20 //scrollTop 是scroll的垂直位置
         }, 1000);
         return false;
     });
 
     $("#main-slider").owlCarousel({
-        // autoPlay: 5000,
+        autoPlay: 5000,
         navigation: true, // Show next and prev buttons
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         slideSpeed: 500,
@@ -20,7 +43,6 @@ $(document).ready(function() {
         responsive: true,
         singleItem: true
     }); // end main-slider
-
 
     $("#owl-artist").owlCarousel({
         items: 4,
